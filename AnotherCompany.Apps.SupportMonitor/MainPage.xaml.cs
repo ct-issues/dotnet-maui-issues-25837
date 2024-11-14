6c2ed5030ad3376dc0414b9eb802c85caa4ba2e3
codetuner <rudi.breedenraedt@arebis.be>
2024-11-14 07:28:20 +0100

    Add project files.

---
+++ AnotherCompany.Apps.SupportMonitor/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AnotherCompany.Apps.SupportMonitor
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
